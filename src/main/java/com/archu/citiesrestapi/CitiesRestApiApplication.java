e14978fca1b14b440c4b7c2927994ee819ab59e8
MarcinPerka <marcinp1596@gmail.com>
2020-07-14 19:27:21 +0200

    init

---
+++ src/main/java/com/archu/citiesrestapi/CitiesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.archu.citiesrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitiesRestApiApplication.class, args);
+    }
+
+}
